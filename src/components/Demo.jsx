3399261d9bc27060f5f8de9f7f57581aeba605f5
YE <ye@pop-os.localdomain>
2023-07-13 16:25:46 +0300

    article summarizer work, history links works, copy to clipboard don't work yet

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,23 +1,37 @@
-import { useEffect, useState  } from "react"
-import { copy, linkIcon, loader, tick  } from "../assets"
-import { useLazyGetSummaryQuery } from "../services/articel"
+import { useEffect, useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/articel";
 function Demo() {
-  const [article, setArticle ] = useState({
-    url:'',
-    summary:''
-  })
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-  const[getSummary,{error,isFitching }]= useLazyGetSummaryQuery()
+  const [allArticles, setAllArticles] = useState([]);
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-    const {data} =await getSummary({articleUrl:article.url})
-    if(data?.summary){
-      const newArticle = {...article,summary:data.summary}
-      setArticle(newArticle)
-      console.log(newArticle)
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("article")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
-  }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -34,12 +48,14 @@ function Demo() {
           <input
             type="url"
             placeholder="Enter a URL"
-            value= {article.url}
+            value={article.url}
             required
-            onChange={(e) => {setArticle({
-              ...article,
-              url: e.target.value
-            })}}
+            onChange={(e) => {
+              setArticle({
+                ...article,
+                url: e.target.value,
+              });
+            }}
             className="peer block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus:border-black focus:outline-none focus:ring-0"
           />
           <button
@@ -50,9 +66,62 @@ function Demo() {
           </button>
         </form>
         {/* Browser URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((article, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(article)}
+              className="p-3 flex justify-start items-center flex-row bg-white border border-gray-200 gap-3 rounded-lg cursor-pointer"
+            >
+              <div className="w-7 h-7 rounded-full bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199, 199, 199, 0.2)] backdrop-blur flex justify-center items-center cursor-pointer">
+                <img
+                  src={copy}
+                  alt="copy-icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {article.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Display Result  */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img
+            src={loader}
+            alt="loading-icon"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen... <br />
+            <span className="font-satoshi font-normal text-gray-700 ">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article{" "}
+                <span className="font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">
+                  Summary
+                </span>
+              </h2>
+              <div className="rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199, 199, 199, 0.2)] backdrop-blur p-4">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
 }
 
-export default Demo
+export default Demo;
